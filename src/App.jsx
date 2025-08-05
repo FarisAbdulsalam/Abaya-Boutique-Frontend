bb1adbf2fd3b5e7015303d567ca56c3528582c37
fatemaalalaiwi <F.alalaiwi@gmail.com>
2025-08-05 19:48:33 +0300

    AbayaDetail,AbayaForm,AbayaList,abayaServices,App.jsx,frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useState, useEffect } from 'react'
+import * as abayaService from './services/abayaService'
+import AbayaList from './components/AbayaList/AbayaList'
+import AbayaForm from './components/AbayaForm/AbayaForm'
+import AbayaDetail from './components/AbayaDetail/AbayaDetail'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const [abayas, setAbaya] = useState([])
+  const [selected, setSelected] = useState(null)
+  const [isFormOpen, setIsFormOpen] = useState(false)
+ 
+//  hook to call the index service function.
+  useEffect(() => {
+    const fetchAbayas = async () => {
+      const fetchedAbaya = await abayaService.index();
+      setAbaya(fetchedAbaya)
+    }
+
+    fetchAbayas();
+  }, []);
+  
+
+  const handleSelect = (abaya) => {
+      setSelected(abaya)
+      setIsFormOpen(false)
+  }
+//ex:  const handleFormView = (pet) => {
+  //   if (!pet._id) setSelected(null);
+  //   setIsFormOpen(!isFormOpen);
+  // };
+  const handleFormView =(abaya) => {
+    if (!abaya._id) {
+    setSelected(null);
+  } else {
+    setSelected(abaya);
+  }
+     setIsFormOpen (!isFormOpen);
+  }
+
+  //هنا نستدعي create() ونضيف العنصر الجديد
+  const handleAddAbaya = async (formData) => {
+    const newAbaya = await abayaService.create(formData)
+    console.log({newAbaya})
+    setAbaya([newAbaya, ...abayas]);
+    setIsFormOpen(false);
+  }
+
+  const handleUpdateAbaya = async (formData, abayaId) => {
+    const updatedAbaya = await abayaService.update(formData ,abayaId);
+    const updatedAbayaList = abayas.map((abaya)=> (
+      abaya._id !== updatedAbaya._id ? abaya : updatedAbaya
+    ));
+    setAbaya(updatedAbayaList);
+    setSelected(updatedAbaya);
+    setIsFormOpen(false);
+
+  }
+
+
+   const handleDeleteAbaya = async (abayaId) =>{
+     const deletedAbaya = await abayaService.deleteAbaya(abayaId) ;
+     setAbaya(abayas.filter(p=>p._id!=abayaId))
+     setIsFormOpen(false)
+     setSelected(null)
+    }
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+    <AbayaList abayas={abayas} handleSelect={handleSelect} handleFormView={handleFormView} isFormOpen={isFormOpen}/>
+    
+    {isFormOpen ? (
+      <AbayaForm handleAddAbaya={handleAddAbaya} handleUpdateAbaya={handleUpdateAbaya} selected={selected} handleDeleteAbaya={handleDeleteAbaya}/>
+
+    ): ( <AbayaDetail selected={selected} handleFormView={handleFormView} handleDeleteAbaya={handleDeleteAbaya}/>)}
+        
+
     </>
   )
 }
-
 export default App
---
+++ src/components/AbayaDetail/AbayaDetail.jsx
@@ -0,0 +1,38 @@
+const AbayaDetail = (props) => {
+  // return if props.selected is null
+  
+  
+  if (!props.selected) {
+    return (
+      <div>
+        <h1>NO DETAILS</h1>
+      </div>
+    );
+  }
+  // return statement if props.selected has a truthy value
+  
+  
+  return (
+    <div>
+         <h1>{props.selected.image}</h1>
+      <h1>{props.selected.title}</h1>
+      <h2>Size: {props.selected.size}</h2>
+      <h2>
+        Quantity: {props.selected.quantity} 
+      </h2>
+       <h2>
+        Price: {props.selected.price} 
+      </h2>
+      <div>
+        <button onClick={()=> props.handleFormView(props.selected)}> Update Abaya</button>
+      </div>
+      
+ <div>
+        <button onClick={()=> props.handleDeleteAbaya(props.selected._id)}> Delete Abaya</button>
+      </div>
+    </div>
+  );
+};
+
+
+export default AbayaDetail; 
